c415d529f73b5b11a8c97c5dbe70261cb3b3b884
chirsworking <chrisjorgework@protonmail.com>
2019-10-20 14:40:08 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAddTech() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
   return (
     <>
-      <h1>Hello world</h1>
-      <h2>VITALVES</h2>
+      {techs.length > 0 && (
+        <ul>
+          {techs.map(tec => (
+            <li key={tec}> {tec} </li>
+          ))}
+        </ul>
+      )}
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAddTech}>
+        Adicionar
+      </button>
     </>
   );
 }
-
-export default App;
